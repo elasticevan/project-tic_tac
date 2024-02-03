d17b1ecb93513d081b830c3edba58c36b9c6d210
Evan Barbeyto <evann.bar@gmail.com>
2024-02-03 15:00:41 -0800

    create algorithm for winning solution

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # project-tic_tac
-tic tac toe game
+
+1. create board
+2. button functions
+3. toggle player function
+4. read for win or draw
+    a. child[1] && child[2] && child[3]; ||
+
+       child[4] && child[5] && child[6]; ||
+
+       child[7] && child[8] && child[9]; ||
+    
+       child[1] && child[4] && child[7]; ||
+
+       child[1] && child[5] && child[9]; ||
+
+       child[2] && child[5] && child[8]; ||
+
+       child[3] && child[6] && child[9]
--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 // JS for Project TTT 
 
 const space = document.querySelectorAll(".space");
-space.forEach(btn => btn.addEventListener("click", () => btn.style.backgroundColor = "red"));
+
+
+function move(player) {
+    space.forEach(btn =>btn.addEventListener("click", () => {
+        player = (player === "one") ? "two" : "one";
+        if (player === "one") {
+            btn.style.backgroundColor = "red";
+        } else if (player === "two") {
+            btn.style.backgroundColor = "blue";
+        } console.log(player)
+    }))
+}
+
+move()
