86832550663e3719e3b73e66954c8012abc5e781
Evan Barbeyto <evann.bar@gmail.com>
2024-02-04 21:16:09 -0800

    create winning formula

--- script.js
+++ script.js
@@ -13,6 +13,31 @@ for (let i = 0; i < 9; i++) {
 const spaces = document.querySelectorAll(".space");
 
 //color/player toggle
+
+
+
+//test for win
+let winners;
+
+for (let i = 0; i <= spaces.length; i++) {
+    winners = [
+        [spaces[0], spaces[1], spaces[2]],
+        [spaces[3], spaces[4], spaces[5]],
+        [spaces[6], spaces[7], spaces[8]],
+        [spaces[0], spaces[3], spaces[6]],
+        [spaces[0], spaces[4], spaces[8]],
+        [spaces[1], spaces[4], spaces[7]],
+        [spaces[2], spaces[5], spaces[8]]
+    ]
+}
+
+function win(){
+    for (let win of winners) {
+        if (win.forEach(space => space.style.backgroundColor = "red") == true)
+        console.log("win")
+    }
+}
+
 function turn(player) {
     spaces.forEach(btn =>btn.addEventListener("click", () => {
         player = (player === "one") ? "two" : "one";
@@ -21,40 +46,8 @@ function turn(player) {
         } else if (player === "two") {
             btn.style.backgroundColor = "blue";
         } console.log(player)
-    }))
+    }));
+    
 }
 
 turn()
-
-
-//test for win
-
-    for (let i = 0; i <= spaces.length; i++) {
-        let color = spaces.style.backgroundColor = "black";
-        color[i]
-    }
-
-/* 
-function winner() {
-    let color = spaces.style.backgroundColor;
-    for (let i = 0; i <= spaces.length; i++) {
-        
-        if (
-            [color[1] === "red" && spaces[2] && spaces[3]] ||
-
-            [spaces[4] && spaces[5] && spaces[6]] ||
-     
-            [spaces[7] && spaces[8] && spaces[9]] ||
-         
-            [spaces[1] && spaces[4] && spaces[7]] ||
-     
-            [spaces[1] && spaces[5] && spaces[9]] ||
-     
-            [spaces[2] && spaces[5] && spaces[8]] ||
-     
-            [spaces[3] && spaces[6] && spaces[9]]
-        )
-
-    }
-}
-*/
